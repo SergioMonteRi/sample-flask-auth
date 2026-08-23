43ea0d1fc9c69c122d22aebbf6890cb633e0b007
SergioMonteRi <sergioribeiropalermo@gmail.com>
2026-08-23 09:46:24 -0300

    feat: add user creation endpoint and validation model

--- app.py
+++ app.py
@@ -2,10 +2,12 @@ from uuid import UUID
 from sqlalchemy import select
 from pydantic import ValidationError
 from flask import Flask, request, jsonify
+from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import LoginManager, login_user, logout_user, login_required, current_user
 
 from database import db
 from models.login_request import LoginRequest
+from models.create_user_request import CreateUserRequest
 
 app = Flask(__name__)
 
@@ -54,16 +56,12 @@ def login():
 
     user = db.session.scalar(stmt)
 
-    if user is None or user.password != password:
+    if user is None or not check_password_hash(user.password, password):
          return jsonify({
             "error": "Invalid username or password"
         }), 401
 
     login_user(user)
-
-    print("USER:", user)
-    print("AUTHENTICATED:", current_user.is_authenticated)
-    print("CURRENT USER:", current_user)
 
     return jsonify({
         "message": "Sucessfull login"
@@ -80,7 +78,30 @@ def logout():
     return jsonify({
         "message": "Sucessfull logout"
     })
-    
+
+@app.route("/create-user", methods=["POST"])
+def create_user():
+    try:
+        data = request.json
+        create_user_data = CreateUserRequest.model_validate(data)
+    except ValidationError as e:
+        return jsonify({
+            "error": "Invalid login data",
+            "details": e.errors()
+        }), 400
+
+    user = User(
+        username=create_user_data.username,
+        password=generate_password_hash(create_user_data.password)
+    )
+
+    db.session.add(user)
+    db.session.commit()
+
+    return jsonify({
+            "message": "User created with sucess"
+    }), 201
+
 
 
 if __name__ == "__main__":
